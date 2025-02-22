5edb1af12a10d3eed880acad35a4405975724c1f
jovicumon <jovicumon@gmail.com>
2025-02-22 10:15:37 +0100

    first commit

---
+++ src/GameCell.java
@@ -0,0 +1,20 @@
+public abstract class GameCell {
+
+    private boolean isDiscovered;
+    protected String content;
+
+    public boolean isDiscovered() {
+        return isDiscovered;
+    }
+
+    public void setDiscovered() {
+        isDiscovered = true;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return  content;
+    }
+}
